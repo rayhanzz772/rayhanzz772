e636e4a50bb12296a09f035ff8792200c05b6a97
rayhan <rayhanzz772@gmail.com>
2025-04-10 09:00:00 +0700

    update 1

--- backdate.sh
+++ backdate.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-# Pastikan sudah di dalam folder repo git
-# git init (kalau belum)
-
 for i in {1..5}; do
   echo "update $i" >> log.txt
   
-  GIT_AUTHOR_DATE="2025-05-10T0$((8+i)):00:00" \
-  GIT_COMMITTER_DATE="2025-05-10T0$((8+i)):00:00" \
+  export GIT_AUTHOR_DATE="2025-04-10T0$((8+i)):00:00"
+  export GIT_COMMITTER_DATE="2025-04-10T0$((8+i)):00:00"
+  
   git add . && git commit -m "update $i"
 done
+
+unset GIT_AUTHOR_DATE
+unset GIT_COMMITTER_DATE
 
 git push origin main
