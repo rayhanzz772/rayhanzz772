4309f0f4fbc23231880aa10654a3fc63c1b6d74d
rayhan <rayhanzz772@gmail.com>
2025-04-11 09:00:00 +0700

    day 11 - update 1

--- backdate.sh
+++ backdate.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-for i in {1..5}; do
-  echo "update $i" >> log.txt
-  
-  export GIT_AUTHOR_DATE="2025-04-10T0$((8+i)):00:00"
-  export GIT_COMMITTER_DATE="2025-04-10T0$((8+i)):00:00"
-  
-  git add . && git commit -m "update $i"
+for day in {11..29}; do
+  for i in {1..5}; do
+    echo "update day $day - $i" >> log.txt
+    
+    export GIT_AUTHOR_DATE="2025-04-${day}T0$((8+i)):00:00"
+    export GIT_COMMITTER_DATE="2025-04-${day}T0$((8+i)):00:00"
+    
+    git add . && git commit -m "day $day - update $i"
+  done
 done
 
 unset GIT_AUTHOR_DATE
