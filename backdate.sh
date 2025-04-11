7ec5d0efe10ec570b37d03b78611b810fc7ca64a
rayhan <rayhanzz772@gmail.com>
2025-04-11 09:00:00 +0700

    day 11 - update 1

--- backdate.sh
+++ backdate.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 for day in {11..29}; do
-  for i in {1..5}; do
+  total=$((RANDOM % 10 + 1))  # random 1-10
+  
+  for i in $(seq 1 $total); do
     echo "update day $day - $i" >> log.txt
     
     export GIT_AUTHOR_DATE="2025-04-${day}T0$((8+i)):00:00"
@@ -9,6 +11,8 @@ for day in {11..29}; do
     
     git add . && git commit -m "day $day - update $i"
   done
+  
+  echo "Day $day: $total commits"
 done
 
 unset GIT_AUTHOR_DATE
