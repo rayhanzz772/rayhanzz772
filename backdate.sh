6274190446e63be5a2a404b8b5a293632f13e75a
rayhan <rayhanzz772@gmail.com>
2025-07-01 09:00:00 +0700

    day 1 - update 1

--- backdate.sh
+++ backdate.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-for day in {3..22}; do
+for day in {1..18}; do
   total=$((RANDOM % 7 + 1))  # random 1-7
   
   for i in $(seq 1 $total); do
     echo "update day $day - $i" >> log.txt
     
-    export GIT_AUTHOR_DATE="2025-05-${day}T0$((8+i)):00:00"
-    export GIT_COMMITTER_DATE="2025-05-${day}T0$((8+i)):00:00"
+    export GIT_AUTHOR_DATE="2025-07-${day}T0$((8+i)):00:00"
+    export GIT_COMMITTER_DATE="2025-07-${day}T0$((8+i)):00:00"
     
     git add . && git commit -m "day $day - update $i"
   done
